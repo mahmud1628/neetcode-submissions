e7b89ff0730f5cee8091513fc67d31d902443818
Abdullah Al Mahmud <151846573+mahmud1628@users.noreply.github.com>
2026-06-11 00:59:53 +0600

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.cpp
@@ -0,0 +1,46 @@
+class MedianFinder {
+    priority_queue<int> max_heap;
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        int min_size = min_heap.size();
+        int max_size = max_heap.size();
+
+        if(min_size == 0 && max_size == 0) {
+            min_heap.push(num);
+            return;
+        }
+
+        int min_top = min_heap.top();
+        int max_top = max_heap.top();
+
+        if(num >= min_top) {
+            min_heap.push(num);
+            min_size++;
+        }
+        else {
+            max_heap.push(num);
+            max_size++;
+        }
+        
+        if(min_size == max_size + 2) {
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+        } else if(max_size == min_size + 2) {
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(min_heap.size() == max_heap.size())
+            return 1.0 * (min_heap.top() + max_heap.top()) / 2;
+        else if(min_heap.size() > max_heap.size())
+            return min_heap.top();
+        else return max_heap.top();
+    }
+};
