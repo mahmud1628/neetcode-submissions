3424ba032c302eb249867c110e98d7b9eab15f42
Abdullah Al Mahmud <151846573+mahmud1628@users.noreply.github.com>
2026-06-14 10:29:47 +0600

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    bool isPalindrome(string s, int start, int end) {
+        while(start <= end) {
+            if(s[start] != s[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+public:
+    int countSubstrings(string s) {
+        int count = 0;
+        for(int i = 0; i < s.size(); i++) {
+            for(int j = i; j < s.size(); j++) {
+                if(isPalindrome(s, i, j)) count++;
+            }
+        }
+        return count;
+    }
+};
