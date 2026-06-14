0430853bdc2682a229671e919e153ba57d16ece8
Abdullah Al Mahmud <151846573+mahmud1628@users.noreply.github.com>
2026-06-14 11:21:02 +0600

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    // checks whether the subtring is a palindrome, pass s as reference otherwise the string is copied for each iteration
+    bool isPalindrome(string & s, int start, int end) {
+        while(start <= end) {
+            if(s[start] != s[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+public:
+    string longestPalindrome(string s) {
+        int max_length = -1;
+        string longest = "";
+
+        for(int i = 0; i < s.size(); i++) {
+            for(int j = i; j < s.size(); j++) {
+                if(isPalindrome(s, i , j)) {
+                    if(j - i + 1 > max_length) {
+                        max_length = j - i + 1;
+                        longest = s.substr(i, max_length);
+                    }
+                }
+            }
+        }
+        return longest;
+    }
+};
