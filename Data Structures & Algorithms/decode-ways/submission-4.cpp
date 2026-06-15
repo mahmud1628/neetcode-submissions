b2af3c2084a828c4d40bb33b1f89088d331afd68
Abdullah Al Mahmud <151846573+mahmud1628@users.noreply.github.com>
2026-06-15 15:12:20 +0600

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    int numDecodings(string & s, int index, vector<int> & mem) {
+        if(index >= s.size()) {
+            return 1;
+        }
+        if(index == s.size() - 1) {
+            return 1;
+        }
+        if(mem[index] == -1) {
+            char ch = s[index];
+            if(ch == '0') {
+                mem[index] = 0;
+                return mem[index];
+            }
+            char next_ch = s[index + 1];
+            if(next_ch == '0' && ch < '3') {
+                mem[index] = numDecodings(s, index + 2, mem);
+            } else {
+                int a = numDecodings(s, index  + 1, mem);
+                int b = 0;
+                if(!(ch >= '2' && next_ch >= '7') && ch < '3') {
+                    b = numDecodings(s, index + 2, mem);
+                }
+                mem[index] = a + b;
+            }
+        }
+        return mem[index];
+    }
+public:
+    int numDecodings(string s) {
+        if(s[0] == '0') return 0;
+        vector<int> mem(s.size(), -1);
+        return numDecodings(s, 0, mem);
+    }
+};
