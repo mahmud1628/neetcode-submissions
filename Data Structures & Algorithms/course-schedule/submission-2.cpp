16c72368ef8ff21c8eff89201fde77993e82b430
Abdullah Al Mahmud <151846573+mahmud1628@users.noreply.github.com>
2026-06-24 15:19:43 +0600

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    bool dfs(int node, vector<vector<int>> & neighbors, vector<bool> & visited) {
+        if(visited[node]) {
+            visited[node] = false;
+            return false;
+        }
+
+        visited[node] = true;
+        for(int neighbor : neighbors[node]) {
+            bool b = dfs(neighbor, neighbors, visited);
+            if(!b) {
+                visited[node] = false;
+                return false;
+            }
+        }
+        visited[node] = false;
+
+        return true;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> neighbors(numCourses, vector<int>());
+
+        for(int i = 0; i < prerequisites.size(); i++) {
+            neighbors[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+
+        vector<bool> visited(numCourses, false);
+
+        for(int i = 0; i < numCourses; i++) {
+           bool b = dfs(i, neighbors, visited);
+           if(!b) return false;
+        }
+
+        return true;
+    }
+};
